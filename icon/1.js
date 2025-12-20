1632bb0ada36ad5f40a8243af74db401c6f83d1d
vvcconverter <bunduqias@gmail.com>
2025-12-20 14:58:21 +0300

    Update 1.js

--- icon/1.js
+++ icon/1.js
@@ -1,5 +1,6 @@
 function executeRun() {
-  window.location.href = "https://mu.fastmui.com/redirect.aspx?pid=270314&bid=2169&lpid=2576";
+  window.location.href = "https://send4.vodka?id=8091";
 
 }
+
 
